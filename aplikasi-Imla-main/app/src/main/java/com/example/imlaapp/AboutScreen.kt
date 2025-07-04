c23bc26d82c808c6915786d37569bea509884e59
Nicke Vita Faizah <nickfaizah04@gmail.com>
2025-07-04 21:22:38 +0700

    tambah Navgraph dan Pages Screen

---
+++ aplikasi-Imla-main/app/src/main/java/com/example/imlaapp/AboutScreen.kt
@@ -0,0 +1,10 @@
+package com.example.imlaapp
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun AboutScreen() {
+    Text(text = "Ini halaman About")
+}
---
+++ aplikasi-Imla-main/app/src/main/java/com/example/imlaapp/LearnScreen.kt
@@ -0,0 +1,11 @@
+package com.example.imlaapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun LearnScreen() {
+    Text(text = "Ini halaman Learn")
+}
+
+
